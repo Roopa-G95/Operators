db6a92b5787aeba6a47980a89634c2e945a6f15a
Roopa-G95 <roopagokulakrishnan95@gmail.com>
2024-11-29 15:28:02 +0530

    Diff_Operators

---
+++ Operators.py
@@ -0,0 +1,47 @@
+a = 10
+b = 5
+print('\n')
+print('Arithmetic Operators')
+print('---------------------')
+print('Addition',a+b)
+print('Subtraction',a-b)
+print('Multiplication',a*b)
+print('Division',a/b)
+print('Modulus',a%b)
+print('Exponential(power of)',a**b)
+print('Round off',a//b)
+print('\n')
+print('Assignment Operator')
+print('---------------------')
+a+=3;print(a)
+a-=3;print(a)
+b*=2;print(b)
+b/=5;print(int(b))
+c=25
+c//=3; print(c)
+d=5
+d**=2;print(d)
+d=5
+d^=2;print(d)
+print('\n')
+print('Comparison Operator')
+print('---------------------')
+a=2;b=5
+print('Check if Equal:',a==b)
+print('Not equal:',a!=b)
+print('Greater than:',a>b)
+print('Lesser than:',a<b)
+x=4
+y=4
+print('Greater than or equal to:',x>=y)
+m=5
+n=2
+print('Lesser than or equal to:',m<=n)
+print('\n')
+print('Logical Operator')
+print('---------------------')
+a=7
+b=1.2
+print('AND operator',a>b and a!=b)
+print('OR Operator',a<b or a<=b or a>b)
+print('Not Operator',not(a>b))
